72471691ca3f2cc2f51176bb649e956288e93e86
LaksCastro <lakscastro@gmail.com>
2021-12-16 22:36:37 -0300

    (#1) Add CLI version of `/core` API

---
+++ cli/line-count.js
@@ -0,0 +1,24 @@
+/// CLI implementation of `HowManyLines` API
+const { default: HowManyLines } = require("how-many-lines");
+
+/// Usage `node index TOKEN=<TOKEN>`
+const args = process.argv
+  .slice(2)
+  .map((arg) => arg.split("="))
+  .reduce((current, pair) => ({ ...current, [pair[0]]: pair[1] }), {});
+
+const token = args.TOKEN;
+
+/// Uses the `HowManyLines` API to generate the `lineCount`
+async function fetchLineCount() {
+  const howManyLines = HowManyLines({ token });
+
+  const stats = await howManyLines.generateStats();
+
+  const lineCount = howManyLines.calcLineCount(stats);
+
+  return lineCount;
+}
+
+/// Trick to write to stdout when API fetch to finish with `async` syntax
+(async () => process.stdout.write(`${await fetchLineCount()}`))();
