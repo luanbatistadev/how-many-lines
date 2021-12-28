a75378ecf0344ebf0825f362d53121f6fcbcd7f9
LaksCastro <lakscastro@gmail.com>
2021-12-27 22:19:03 -0300

    (#1) Add `howmanylines` `webservice` using `sls`

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const rateLimit = require("express-rate-limit");
+require(`dotenv/config`);
+const serverless = require(`serverless-http`);
+const express = require(`express`);
+const rateLimit = require(`express-rate-limit`);
+const fetch = require(`node-fetch`);
+const { default: HowManyLines } = require(`how-many-lines`);
 
 const app = express();
+
+const { REPOSITORY, REPO_TOKEN } = process.env;
+
+const OPEN_ISSUE_EVENT = "open-issue";
 
 /// Apply rate limit of 2 request / 15 minutes
 app.use(
@@ -13,15 +20,52 @@ app.use(
 );
 
 /// Parse and limit the request body
-app.use(express.json({ limit: "100kb" }));
+app.use(express.json({ limit: `100kb` }));
 
-/// Endpoint that accept requests here
+/// Endpoint that accept requests
 /// to trigger the workflow on the repository
-app.post("/", async function (req, res) {
+app.post(`/dispatches`, async function (req, res) {
   if (!req.body.token) return res.status(400).send(`Missing token`);
 
-  /// TODO: Implement
-  /// dispatchGithubWorkflowEvent({ payload: { token: req.body.token } });
+  async function dispatchGithubWorkflowEvent(payload) {
+    const response = await fetch(`https://api.github.com/user`, {
+      headers: {
+        authorization: `Token ${payload.token}`,
+      },
+    });
+
+    if (response.status !== 200) return res.status(401).send(`Invalid token!`);
+
+    const result = await fetch(
+      `https://api.github.com/repos/${REPOSITORY}/dispatches`,
+      {
+        method: `POST`,
+        body: JSON.stringify({
+          event_type: OPEN_ISSUE_EVENT,
+          client_payload: payload,
+        }),
+        headers: {
+          authorization: `Token ${REPO_TOKEN}`,
+        },
+      }
+    );
+
+    return res.sendStatus(result.status);
+  }
+
+  await dispatchGithubWorkflowEvent({ token: req.body.token });
+});
+
+/// Endpoint that accept requests to run `HowManyLines` core package
+app.post(`/`, async function (req, res) {
+  if (!req.body.token) return res.status(400).send(`Missing token`);
+
+  const how = HowManyLines({ token: req.body.token });
+
+  const stats = await how.generateStats();
+  const lineCount = how.calcLineCount(stats);
+
+  return res.send(`${lineCount}`);
 });
 
 /// Serverless handler [refer to details](https://www.serverless.com/blog/serverless-express-rest-api/)
